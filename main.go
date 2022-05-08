e6154fc5f4ff826018548a8583f856ddbf79564a
João Albuquerque <joaopasantos0712@gmail.com>
2022-05-08 12:10:08 -0300

    Implementa Selection Sort e Insertion Sort

--- main.go
+++ main.go
@@ -10,6 +10,6 @@ func main() {
 	// var array []int
 	array := []int{6, 9, 4, 8, 7, 5, 10, 3, 2, 1}
 	fmt.Println(array)
-	sorters.Bubble(array)
+	sorters.Insertion(array)
 	fmt.Println(array)
 }
--- sorters/sorters.go
+++ sorters/sorters.go
@@ -12,12 +12,32 @@ func Bubble(array []int) {
 	}
 }
 
-func Selection() {
-
+func Selection(array []int) {
+	for i := 0; i < len(array)-1; i++ {
+		min := i
+		for j := i + 1; j < len(array); j++ {
+			if array[min] > array[j] {
+				min = j
+			}
+		}
+		if i != min {
+			temp := array[i]
+			array[i] = array[min]
+			array[min] = temp
+		}
+	}
 }
 
-func Insertion() {
-
+func Insertion(array []int) {
+	for j := 1; j < len(array); j++ {
+		temp := array[j]
+		i := j - 1
+		for i >= 0 && temp < array[i] {
+			array[i+1] = array[i]
+			i--
+		}
+		array[i+1] = temp
+	}
 }
 
 func Quick() {
