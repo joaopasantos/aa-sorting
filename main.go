1ae463e6b2dd72efd60bd8d19440d68c20aa519f
João Albuquerque <joaopasantos0712@gmail.com>
2022-05-08 17:11:26 -0300

    Implementa interface para inserção do vetor e seleção do método

--- main.go
+++ main.go
@@ -7,10 +7,52 @@ import (
 )
 
 func main() {
-	// var array []int
-	array := []int{6, 9, 4, 8, 7, 5, 10, 3, 2, 1}
-	fmt.Println(array)
-	// sorters.Insertion(array)
-	sorters.Quick(array, 0, len(array)-1)
-	fmt.Println(array)
+	var array []int
+	array = inputNumbers(array)
+	fmt.Println("- Vetor inserido:", array)
+
+	showMenu()
+	var method int
+	var inputIsValid bool
+	for !inputIsValid {
+		inputIsValid = true
+		fmt.Print("Insira o método de ordenação que deseja utilizar: ")
+		fmt.Scanf("%d", &method)
+		switch method {
+		case 1:
+			sorters.Bubble(array)
+		case 2:
+			sorters.Selection(array)
+		case 3:
+			sorters.Insertion(array)
+		case 4:
+			sorters.Quick(array, 0, len(array)-1)
+		default:
+			fmt.Println("Opção Inválida.")
+			inputIsValid = false
+		}
+	}
+	fmt.Println("- Vetor ordenado:", array)
+}
+
+func inputNumbers(array []int) []int {
+	for i := 1; i <= 10; i++ {
+		fmt.Printf("Insira o número que ocupará a posição %d do vetor: ", i)
+		var number int
+		_, err := fmt.Scanf("%d", &number)
+		if err != nil {
+			panic(err)
+		}
+		array = append(array, number)
+	}
+	return array
+}
+
+func showMenu() {
+	fmt.Println("\nMétodos de Ordenação")
+	fmt.Println("[1] - Bubble Sort")
+	fmt.Println("[2] - Selection Sort")
+	fmt.Println("[3] - Insertion Sort")
+	fmt.Println("[4] - Quick Sort")
+	fmt.Println("[0] - Exit")
 }
